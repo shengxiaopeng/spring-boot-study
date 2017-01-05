c7c5363577b6063c4e1d73dda0d85bdf4ee250d6
sxp <sxp>
2017-01-05 22:15:07 +0800

    spring boot mvc

--- src/test/java/com/example/DemoApplicationTests.java
+++ src/test/java/com/example/DemoApplicationTests.java
@@ -1,9 +1,18 @@
 package com.example;
 
+import com.example.transaction.TransactionTest;
+import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.web.client.RestTemplate;
+import org.springframework.web.context.WebApplicationContext;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
@@ -30,8 +39,9 @@ public class DemoApplicationTests {
 	}
 
 
-
-
+    /**
+	 * test mvc
+	 */
 	@Autowired
 	WebApplicationContext webApplicationContext;
 
@@ -52,6 +62,9 @@ public class DemoApplicationTests {
 
 	}
 
+	/**
+	 * restTemplate
+     */
 	@Test
 	public  void  test1(){
 		RestTemplate restTemplate=new RestTemplate();
